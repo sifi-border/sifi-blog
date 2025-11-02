d988fca652eaa03bd41c9edd2ca722b61fa73cfb
sifi-border <shusaku.fujihara@access-company.com>
2025-11-02 18:48:16 +0900

    replace favicon with original image

--- src/config.ts
+++ src/config.ts
@@ -36,6 +36,9 @@ export const siteConfig: SiteConfig = {
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		// }
+		{
+			src: "/favicon/my-avatar.png",
+		}
 	],
 };
 
